ea4120b0d543c0db3c262d67716f0ef9c612952d
Nishu4403 <nishathsulikeri@gmail.com>
2024-01-21 13:07:51 +0530

    added jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    environment {
+        GIT_REPO_URL = 'https://github.com/Nishu4403/d1.git'
+        NGINX_PATH = 'C:\\Users\\nisha\\OneDrive\\Desktop\\jenk\\nginx-1.24.0\\htmldocs'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    // Use the checkout step to clone the Git repository
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git1', url: 'https://github.com/Nishu4403/d1.git']]])
+                }
+            }
+        }
+
+        stage('Deploy to Nginx') {
+            steps {
+                script {
+                    // Using the Jenkins workspace variable to reference files
+                    bat 'xcopy /y "C:\\Users\\nisha\\OneDrive\\Desktop\\d1\\index.html" "%NGINX_PATH%"'
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline succeeded! You can add additional steps here.'
+        }
+        failure {
+            echo 'Pipeline failed! You may want to take some actions.'
+        }
+    }
+}
+
